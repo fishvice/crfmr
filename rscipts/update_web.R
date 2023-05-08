4bfcf4a2371528b83583432075afd303c515f3b2
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-05-08 09:28:47 +0000

    update yml and things

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -1,2 +1,2 @@
-system("cp -r _site /net/www/export/home/hafri/einarhj/public_html/crfmr/.")
+system("cp -r _site/* /net/www/export/home/hafri/einarhj/public_html/crfmr/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr")
