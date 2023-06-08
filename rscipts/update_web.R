5fb4ecb6de9d9fb2c8c7efc913a45892d7912789
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-06-08 11:28:12 +0000

    update

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -1,2 +1,5 @@
 system("cp -r _site/* /net/www/export/home/hafri/einarhj/public_html/crfmr/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr")
+
+system("cp -r aux/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
+system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
