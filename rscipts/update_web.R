fdbb8922b6a1a340c25eaf74c8bd1082b01caaa4
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-06-08 21:41:00 +0000

    first day - ready to roll

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -1,5 +1,7 @@
 system("cp -r _site/* /net/www/export/home/hafri/einarhj/public_html/crfmr/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr")
 
+system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/.")
+
 system("cp -r aux/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
