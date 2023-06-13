da028bba4095baceb1ae98d0a87dcbbfd778071e
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-06-13 08:51:01 +0000

    revamping transformation1

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -6,4 +6,3 @@ system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/."
 system("cp -r aux/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
 
-
