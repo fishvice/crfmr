33fec6088ecc950b353de1b1fce741ecc806f751
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-06-14 20:22:20 +0000

    add some case examples

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -6,3 +6,4 @@ system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/."
 system("cp -r aux/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
 
+
