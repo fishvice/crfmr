7312e6f7fd17ac5d54801c687bf7d1019cc56d0f
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-06-24 04:42:26 +0000

    update

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -3,6 +3,8 @@ system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr")
 
 system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/.")
 
+
+system("cp -r ignored-dir/CMSY/surplus-production_models_GRO-ftp.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("cp -r ignored-dir/CMSY/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
 
