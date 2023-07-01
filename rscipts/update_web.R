d629fbb68b8fc5e73f865b08c754519079fe9090
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-07-01 18:25:37 +0000

    end of course version - likely reproducable

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -5,7 +5,11 @@ system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/."
 
 
 system("cp -r ignored-dir/CMSY/surplus-production_models_GRO-ftp.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
-system("cp -r ignored-dir/CMSY/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
+system("cp -r ignored-dir/LB-SPR-GRO-FTP-27-06-23.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
+
+
+system("cp -r ignored-dir/CMSY/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
+
 
 
