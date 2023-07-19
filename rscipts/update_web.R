cc9a8133466fde0f32ffa0bc14dde4151b2cd420
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-07-19 16:31:57 +0000

    maps.qmd

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -3,13 +3,12 @@ system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr")
 
 system("cp fao_report.Rmd /net/www/export/home/hafri/einarhj/public_html/data/.")
 
-
+system("cp -r ignored-dir/connecting-r-to-ms-access-database.html /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("cp -r ignored-dir/CMSY/surplus-production_models_GRO-ftp.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
+system("cp -r ignored-dir/derrick/presentation/How-do-I-get-my-MS-Access-data-into-R.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("cp -r ignored-dir/LB-SPR-GRO-FTP-27-06-23.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
 
 
 system("cp -r ignored-dir/CMSY/CMSY++.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
-
-
 
