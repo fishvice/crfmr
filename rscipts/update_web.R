d220996e37077b6a689c50b6e7e63ae976a4b186
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-08-08 14:40:08 +0000

    added the length-distribution example

--- rscipts/update_web.R
+++ rscipts/update_web.R
@@ -8,6 +8,7 @@ system("cp -r ignored-dir/connecting-r-to-ms-access-database.html /net/www/expor
 system("cp -r ignored-dir/CMSY/surplus-production_models_GRO-ftp.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("cp -r ignored-dir/derrick/presentation/How-do-I-get-my-MS-Access-data-into-R.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("cp -r ignored-dir/LB-SPR-GRO-FTP-27-06-23.pptx /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
+system("cp -r ignored-dir/length_exampledata.csv /net/www/export/home/hafri/einarhj/public_html/crfmr/aux/.")
 system("chmod -R a+rX /net/www/export/home/hafri/einarhj/public_html/crfmr/aux")
 
 
